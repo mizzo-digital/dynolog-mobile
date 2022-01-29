ab9ddd824176f7bebcbef1271e3b548b80b8cc83
Thiago Rocha <thiagorocha010@gmail.com>
2022-01-28 22:48:59 -0300

    feat: add disabled state to button

--- src/shared/components/button/index.tsx
+++ src/shared/components/button/index.tsx
@@ -7,15 +7,17 @@ import * as S from './styles';
 
 type Props = TouchableOpacityProps & {
   isLoading?: boolean;
+  disabled?: boolean;
 };
 
 export function Button({
   isLoading,
   children,
+  disabled = false,
   ...rest
 }: PropsWithChildren<Props>) {
   return (
-    <S.Container {...rest}>
+    <S.Container disabled={isLoading || disabled} {...rest}>
       {isLoading && <Spinner />}
 
       {!isLoading && <S.Label>{children}</S.Label>}
--- src/shared/components/button/styles.ts
+++ src/shared/components/button/styles.ts
@@ -1,15 +1,27 @@
-import styled, { css } from 'styled-components/native';
+import styled, { css, DefaultTheme } from 'styled-components/native';
 import { Text } from '..';
 
-export const Container = styled.TouchableOpacity`
-  ${({ theme }) => css`
+const makeBackgroundButton = (theme: DefaultTheme, disabled: boolean) => {
+  if (disabled) {
+    return theme.colors.buttons.solid.primary.disabled;
+  }
+
+  return theme.colors.buttons.solid.primary.enabled;
+};
+
+type ContainerProps = {
+  disabled: boolean;
+};
+
+export const Container = styled.TouchableOpacity<ContainerProps>`
+  ${({ theme, disabled }) => css`
     width: 100%;
     height: 56px;
 
     justify-content: center;
     align-items: center;
 
-    background: ${theme.colors.buttons.solid.primary.enabled};
+    background: ${makeBackgroundButton(theme, disabled)};
     border-radius: ${theme.radius.md};
   `}
 `;
