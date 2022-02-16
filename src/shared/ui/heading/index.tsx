74249030221e06c8b3abfe9e03e34cbb6bc3f4d3
Thiago Rocha <thiagorocha010@gmail.com>
2022-02-15 22:14:48 -0300

    feat: add heading component

---
+++ src/shared/ui/heading/index.tsx
@@ -0,0 +1,30 @@
+import React, { PropsWithChildren } from 'react';
+import { TextProps } from 'react-native';
+
+import { Theme } from '../../styles/themes';
+
+import * as S from './styles';
+
+export type Sizes = keyof Theme['typography']['heading']['fontSizes'];
+export type Colors = keyof Theme['colors']['texts']['heading'];
+export type Fonts = keyof Theme['typography']['heading']['fonts'];
+
+type Props = TextProps & {
+  size?: Sizes;
+  font?: Fonts;
+  color?: Colors;
+};
+
+export function Heading({
+  size = 'md',
+  color = 'primary',
+  font = 'heading-primary-medium',
+  children,
+  ...rest
+}: PropsWithChildren<Props>) {
+  return (
+    <S.Container font={font} color={color} size={size} {...rest}>
+      {children}
+    </S.Container>
+  );
+}
