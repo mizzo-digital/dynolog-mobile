01efa3ba58024ee97713ee186979b398d16b1c9b
Thiago Rocha <thiagorocha010@gmail.com>
2022-02-15 22:14:05 -0300

    feat: add link component

---
+++ src/shared/ui/link/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import { Text } from '..';
+
+export const Container = styled.TouchableOpacity``;
+
+export const TextLink = styled(Text).attrs(() => ({
+  size: 'xs',
+  font: 'body-primary-semibold',
+}))``;
